1764763e8c9ea6006c99c50869fa4667d59a3e69
Luke Patterson <lukewpatterson@gmail.com>
2013-04-26 14:39:44 -0500

    Update project.js

--- project.js
+++ project.js
@@ -5,7 +5,7 @@ angular.module('project', ['mongolab']).
       when('/', {controller:ListCtrl, templateUrl:'list.html'}).
       when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
       when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
-      otherwise({redirectTo:'real_404.html'});
+      otherwise({tempalteUrl:'real_404.html'});
   });
  
  
